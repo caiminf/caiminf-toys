77ea93c0bdadd1c9278a6fc1bf2b79606021cb19
caiminf <920722837@qq.com>
2018-03-31 22:20:46 +0800

    mutex

--- main.cc
+++ main.cc
@@ -2,18 +2,36 @@
 #include <stdlib.h>
 
 #include "components/Thread.h"
+#include "components/MutexLock.h"
 
-void *thread_body(void *arg)
+long g_sum = 0;
+MutexLock g_mutex;
+
+void *thread_body1(void *arg)
 {
-    printf("abc\n");
+    MutexLockGuard lockGuard(g_mutex);
+    for (int i = 0; i < 1000; ++i)
+    {
+        g_sum++;
+    }
+    return NULL;
+}
+
+void *thread_body2(void *arg)
+{
+    MutexLockGuard lockGuard(g_mutex);
+    for (int i = 0; i < 100; ++i)
+    {
+        g_sum++;
+    }
     return NULL;
 }
 
 int main()
 {
-    for (int i = 0; i < 10; ++i)
-    {
-        Thread thread(thread_body);
-    }
+    Thread thread1(thread_body1);
+    Thread thread2(thread_body2);
+
+    printf("g_sum = %d\n", g_sum);
     return 0;
 }
